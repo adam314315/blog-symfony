575b72297f054261f9041d496fef747d4fcd0237
adam314315 <adam314315@gmail.com>
2018-08-17 17:33:20 +0000

    Updated Author model

---
+++ src/Migrations/Version20180817131723.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180817131723 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE author ADD email VARCHAR(255) NOT NULL, ADD is_active TINYINT(1) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BDAFD8C8F85E0677 ON author (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BDAFD8C8E7927C74 ON author (email)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_BDAFD8C8F85E0677 ON author');
+        $this->addSql('DROP INDEX UNIQ_BDAFD8C8E7927C74 ON author');
+        $this->addSql('ALTER TABLE author DROP email, DROP is_active');
+    }
+}
